f534cbdf3ca9fdf228132efdfed3fba95601af01
Leo Carrati <carrati3@gmail.com>
2022-07-25 22:16:57 -0300

    terminei os exercicios

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+const nomeDoUsuario = prompt(`Qual o seu nome?`)
+const comidaFav1 = prompt(`Qual sua comida favorita?`)
+const comidaFav2 = prompt(`Qual sua segunda comida favorita?`)
+const comidaFav3 = prompt(`Qual sua terceira comida favorita?`)
+
+console.log(`Estas são as comidas favoritas de ${nomeDoUsuario}
+- ${comidaFav1}
+- ${comidaFav2}
+- ${comidaFav3}`)
