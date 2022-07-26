eae5c4615414a4929ff33c25db1c764776bdad07
Leo Carrati <carrati3@gmail.com>
2022-07-26 18:30:57 -0300

    arrumei o código

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -2,11 +2,10 @@ const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      
 
 
 // a) Remova o excesso de espaços no final da string;
-let minhaStringMod = minhaString.trim() 
-console.log(minhaStringMod);
+console.log(minhaString.trim());
 
 // b) exiba, em um console.log() a quantidade de caracteres da string, antes e depois da remoção dos espaços;
-console.log(`A frase antes da remoção dos espaços continha ${minhaString.length} e após a remoção ${minhaStringMod.length}`);
+console.log(`A frase antes da remoção dos espaços continha ${minhaString.length} e após a remoção ${minhaString.trim().length}`);
 
 // c) Substitua os traços `________` por “sticioso”.
-console.log(minhaStringMod.replaceAll("________", "sticioso"));
+console.log((minhaString.trim()).replaceAll("________", "sticioso"));
